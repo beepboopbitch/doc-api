ae1cdd970b734c1e981b2f9129faf51337997403
Nayomi Morita <nayomi.morita@gmail.com>
2019-03-22 10:32:06 -0700

    i cant figure out how to get outside the meta in the api call

--- src/main.js
+++ src/main.js
@@ -5,6 +5,17 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 $(document).ready(function(){
-  let request = new XMLHttpRequest();
-  const url = `https://api.betterdoctor.com/2016-03-01/doctors?location=45.51,-122.65,11&user_key=${process.env.apiKey}`;
+  $('#docSpecialty').click(function(){
+    const specialty = $('#specialty').val();
+    $('#specialty').val();
+
+    let request = new XMLHttpRequest();
+    const apiKey = process.env.apiKey;
+    const url = `https://api.betterdoctor.com/2016-03-01/doctors?location=45.51,-122.65,11&user_key=${apiKey}`;
+
+
+
+
+
+  });
 });
