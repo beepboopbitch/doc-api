e34675e46bace9efdb168acb9e1e75854f1a7c59
Student <example@example.com>
2019-03-28 13:27:24 -0700

    add ability to search doctor names

--- src/business.js
+++ src/business.js
@@ -1,11 +1,11 @@
 
 export class Doc {
 
-  getDoc(specialty) {
+  getDoc(specialty, name) {
     const apiKey = process.env.exports.apiKey;
     return new Promise(function(resolve, reject){
     let request = new XMLHttpRequest();
-    const url = `https://api.betterdoctor.com/2016-03-01/doctors?location=45.51,-122.65,11&user_key=${apiKey}&specialty_uid=${specialty}`;
+    const url = `https://api.betterdoctor.com/2016-03-01/doctors?location=45.51,-122.65,11&user_key=${apiKey}&specialty_uid=${specialty}&name=${name}`;
     request.onload = function() {
       if (this.status === 200) {
         resolve(request.response);
--- src/main.js
+++ src/main.js
@@ -7,17 +7,18 @@ import { Doc } from './business.js'
 
 $(document).ready(function(){
 
-  function findDoc(specialty){
+  function findDoc(specialty, name){
     const docs = new Doc;
-    const promise = docs.getDoc(specialty);
+    const promise = docs.getDoc(specialty, name);
     promise.then(function(response){
       const body = JSON.parse(response);
       const checkData = docs.checkResults(body);
+      console.log()
       if (checkData === false) {
-        $(".errors").text(`Cant't find doctors who specialize in ${specialty}`);
+        $(".errors").text(`Cant't find doctors based on ${specialty} ${name}`);
       } else if (checkData === true) {
         $(".errors").text(``);
-        $('.doc1').text(`List of Doctors who specialize in that field:`);
+        $('.doc1').text(`List of Doctors:`);
         $('.doc1Name').text(`${(body.data[0]).profile.first_name} ${(body.data[0]).profile.last_name}`);
         $('.doc1Phone').text(`${(((body.data[0]).practices[0]).phones[0]).number}`);
         $('.doc1St').text(`${((body.data[0]).practices[0]).visit_address.street}`);
@@ -38,13 +39,12 @@ $(document).ready(function(){
         $('.doc3St').text(`${((body.data[2]).practices[0]).visit_address.street}`);
         $('.doc3Zip').text(`${((body.data[2]).practices[0]).visit_address.zip}`);
         $('.doc3Accept').text(`Accepts new patients: ${((body.data[2]).practices[0]).accepts_new_patients}`);
-
       }
     });
   }
   $('#docSpecialty').click(function(){
     const specialty = $('#specialty').val();
-    findDoc(specialty);
+    const name = $('#docSearch').val();
+    findDoc(specialty, name);
   });
-
 });
